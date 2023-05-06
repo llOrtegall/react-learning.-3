96155a1cfd6d0e561b6c13a34223504697f23a05
llOrtegall <IvanOrtega_97@hotmail.com>
2023-05-06 01:35:42 -0500

    desde 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
-import "./App.css";
-import { Movies } from "./components/Movies";
-import { useMovies } from "./hooks/useMovies.js";
-import { useState } from "react";
+import './App.css';
 
 function App() {
-  const { movies: mappedMovies } = useMovies();
-  const [query, setQuery] = useState("");
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log({ query });
-    // const fields = Object.fromEntries(new window.FormData(evt.target));
-  };
-
-  const handleChange = (evt) => {
-    setQuery(evt.target.value);
-  };
-
-  return (
-    <div className="page">
-      <header>
-        <h1>Buscardor De Peliculas</h1>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            onChange={handleChange}
-            value={query}
-            name="query"
-            placeholder="Avengers, Star Wars, The Matrix etc ..."
-          />
-          <button type="submit"> Buscar </button>
-        </form>
-      </header>
-
-      <main>
-        <Movies movies={mappedMovies} />
-      </main>
-    </div>
-  );
+    return(
+        <h1>Prueba Tecnica</h1>
+    )
 }
 
 export default App;
