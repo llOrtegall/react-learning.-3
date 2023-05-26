b96528040f421a1296a9c121475ad2b0710aba7d
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-26 18:40:41 -0500

    modulando el fecth y todo lo demas

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,32 +1,13 @@
-import withResults from "../mocks/with-result.json";
-import withOutResults from "../mocks/no-result.json";
-import { useState } from 'react'
+import { useState } from 'react';
+import { searchMovies } from '../services/movies.js'
 
 export function useMovies({ search }) {
-  const API_MOVIES = `https://www.omdbapi.com/?apikey=4fd5a756&s=${search}`;
-  const [responseMovies, setResponseMovies] = useState([])
+  const [movies, setMovies] = useState([])
 
-  const movies = responseMovies.Search;
-
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    image: movie.Poster
-  }))
-
-  const getMovies = () => {
-    if (search) {
-      //setResponseMovies(withResults)
-      fetch(API_MOVIES)
-        .then( res => res.json())
-        .then(json => {
-          setResponseMovies(json);
-        })
-    } else {
-      setResponseMovies(withOutResults)
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search });
+    setMovies(newMovies)
   }
 
-  return { movies: mappedMovies, getMovies }
+  return { movies, getMovies }
 }
---
+++ src/services/movies.js
@@ -0,0 +1,23 @@
+
+export const searchMovies = async ({ search }) => {
+
+	const API_KEY = '4fd5a756';
+
+	if (search === '') return null;
+
+	try {
+		const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`);
+		const json = await response.json();
+		const movies = json.Search;
+
+		return movies?.map(movie => ({
+			id: movie.imdbID,
+			title: movie.Title,
+			year: movie.Year,
+			image: movie.Poster
+		}))
+	} catch (err) {
+		throw new Error('Error while fetching movies')
+	}
+
+}
