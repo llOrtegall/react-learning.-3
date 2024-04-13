e0eff35f0cc17d095250ac60916642b86b8af4c4
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-04-12 21:50:27 -0500

    react router dom agregado

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './auth/AuthProvider.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
+const root = document.getElementById('root')
+
+if (root !== null) {
+  createRoot(root)
+    .render(
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    )
+}
